6d418908000363f81480a60ef4c2f4d963ee3a62
Yogesh Sonawane <ysonawan@akamai.com>
2026-01-05 10:36:38 +0530

    portfolio metrics added to home page

--- scripts/others/deploy.sh
+++ scripts/others/deploy.sh
@@ -3,14 +3,14 @@
 echo "Starting deployment..."
 
 echo "Copying JAR to remote server..."
-scp target/fam-vest-app-2.0.0.jar root@172.105.40.137:~
+scp target/famvest-app-2.0.0.jar root@172.105.40.137:~
 if [ $? -ne 0 ]; then
   echo "Error: SCP failed."
   exit 1
 fi
 
-echo "Moving JAR to /opt/app/fam-vest-app and restarting service..."
-ssh root@172.105.40.137 "sudo mv ~/fam-vest-app-2.0.0.jar /opt/app/fam-vest-app/ && sudo systemctl restart fam-vest-app.service"
+echo "Moving JAR to /opt/app/famvest-app and restarting service..."
+ssh root@172.105.40.137 "sudo mv ~/famvest-app-2.0.0.jar /opt/app/famvest-app/ && sudo systemctl restart famvest-app.service"
 if [ $? -ne 0 ]; then
   echo "Error: SSH command failed."
   exit 1
